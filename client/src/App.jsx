0463c46986c453830e595665ce8fe6d52fd3e251
daniel <daniel16parrillas@gmail.com>
2024-09-23 20:29:53 -0600

    feat: :sparkles: inicialización de proyecto

---
+++ client/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:4000');
+
+function App() {
+  const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  // Unirse a una sala
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('join_room', room);
+    }
+  };
+
+  // Enviar mensaje
+  const sendMessage = () => {
+    if (message !== '') {
+      const messageData = {
+        room,
+        message,
+      };
+
+      socket.emit('send_message', messageData);
+      setMessages([...messages, message]);
+      setMessage(''); // Limpiar el campo del mensaje
+    }
+  };
+
+  // Escuchar los mensajes de la sala
+  useEffect(() => {
+    socket.on('message', (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="p-10">
+      <h1>Chat en Tiempo Real</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="ID de la sala..."
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <button onClick={joinRoom}>Unirse a la sala</button>
+      </div>
+
+      <div className='p-4'>
+        <input
+          type="text"
+          placeholder="Mensaje..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Enviar Mensaje</button>
+      </div>
+
+      <div>
+        <h3>Mensajes:</h3>
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+app.use(cors())
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado:', socket.id)
+
+  socket.on('join_room', (room) => {
+    socket.join(room)
+    console.log(`Usuario con ID: ${socket.id} se unió a la sala: ${room}`)
+
+    socket.emit('message', `Bienvenido a la sala ${room}`)
+  })
+
+  socket.on('send_message', (data) => {
+    io.to(data.room).emit('message', data.message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado:', socket.id)
+  })
+})
+
+const PORT = 4000
+server.listen(PORT, () =>
+  console.log(`Servidor corriendo en el puerto ${PORT}`)
+)
