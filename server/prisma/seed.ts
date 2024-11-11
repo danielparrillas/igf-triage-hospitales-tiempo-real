1ddc6ed38d3e5355395ce2a4c52347711aab2079
danielparrillas <daniel16parrillas@gmail.com>
2024-11-11 09:01:53 -0600

    feat: :seedling: agregar seeder para doctores y pacientes

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,13 +1,86 @@
 import { PrismaClient } from '@prisma/client'
-
+import bcrypt from 'bcrypt'
 import pacientesData from './seeds/pacientes.json'
+import doctoresData from './seeds/doctores.json'
+import enfermerosData from './seeds/enfermeros.json'
+import { RoleEnum } from '../src/types/roleEnum'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.paciente.createMany({
-    data: pacientesData
-  })
+  for (const pacienteData of pacientesData) {
+    await prisma.paciente.upsert({
+      where: { id: pacienteData.id },
+      update: pacienteData,
+      create: pacienteData
+    })
+  }
+
+  for (const userData of doctoresData) {
+    const user = await prisma.user.upsert({
+      where: { email: userData.email },
+      update: {
+        email: userData.email,
+        password: await bcrypt.hash(userData.password, 10),
+        role: RoleEnum.DOCTOR
+      },
+      create: {
+        email: userData.email,
+        password: await bcrypt.hash(userData.password, 10),
+        role: RoleEnum.DOCTOR
+      }
+    })
+
+    const { doctor } = userData
+
+    await prisma.doctor.upsert({
+      where: { userId: user.id },
+      update: {
+        dui: doctor.dui,
+        nombre: doctor.nombre
+      },
+      create: {
+        dui: doctor.dui,
+        user: {
+          connect: { id: user.id }
+        },
+        nombre: doctor.nombre
+      }
+    })
+  }
+
+  for (const userData of enfermerosData) {
+    const user = await prisma.user.upsert({
+      where: { email: userData.email },
+      update: {
+        email: userData.email,
+        password: await bcrypt.hash(userData.password, 10),
+        role: RoleEnum.ENFERMERO
+      },
+      create: {
+        email: userData.email,
+        password: await bcrypt.hash(userData.password, 10),
+        role: RoleEnum.ENFERMERO
+      }
+    })
+
+    const { enfermero } = userData
+
+    await prisma.enfermero.upsert({
+      where: { userId: user.id },
+      update: {
+        dui: enfermero.dui,
+        nombre: enfermero.nombre
+      },
+      create: {
+        dui: enfermero.dui,
+        user: {
+          connect: { id: user.id }
+        },
+        nombre: enfermero.nombre
+      }
+    })
+  }
 }
 
 main()
---
+++ server/src/types/roleEnum.ts
@@ -0,0 +1,6 @@
+export enum RoleEnum {
+  USER = 0,
+  ADMIN = 1,
+  ENFERMERO = 2,
+  DOCTOR = 3
+}
